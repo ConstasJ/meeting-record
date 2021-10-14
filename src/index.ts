1dc21b416532a869c0cfbbdc2c64e9d9880c5423
ConstasJ <2020212726@qq.com>
2021-10-14 20:35:18 +0800

    Many Changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import {Context} from 'koishi-core'
+import {Context,Session} from 'koishi-core'
 
 export const name='meeting-record'
 
+function isChannel({session}){
+    if(!session.channel){
+        return '请在群聊中使用此命令！'
+    }
+}
 export let apply=(ctx:Context) =>{
-    
+    ctx.command('meet')
+        .check(isChannel)
 }
