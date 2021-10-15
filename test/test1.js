bd435500df4b3c5122e07f406e8207ef13f97bef
ConstasJ <2020212726@qq.com>
2021-10-15 21:30:16 +0800

    try some Unit Test

--- test/test1.js
+++ test/test1.js
@@ -2,3 +2,8 @@ const {mocha, MockedApp} =require('koishi-test-utils')
 
 const {}
 const app=new MockedApp()
+const session=app.createSession('user',123)
+
+test('printplatform',async ()=>{
+    session.shouldHaveResponse('','')
+})
