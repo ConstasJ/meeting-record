0eb1cb42948fcc801176386c06aff4b0c12aa299
ConstasJ <2020212726@qq.com>
2021-10-15 18:08:46 +0800

    cancel .options

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ export const name='meeting-record'
 
 export let apply=(ctx:Context) =>{
     ctx.command('meet')
-        .option('introduce','-i 添加会议介绍')
-        .option('subcon','-sc 添加会议部分总结')
-        .option('conclusion','-c 添加会议总结')
+        // .option('introduce','-i 添加会议介绍')
+        // .option('subcon','-sc 添加会议部分总结')
+        // .option('conclusion','-c 添加会议总结')
         .action(({session,options})=>{
             let id=session?.channel
         }
