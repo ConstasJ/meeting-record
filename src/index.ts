954a2ef18dc0fea84c3aed56300e139ba9d56d51
ConstasJ <2020212726@qq.com>
2021-10-15 21:28:37 +0800

    Mainly add Onebot recorder

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,16 @@ declare module 'koishi-core'{
         isMeeting:number
     }
 }
+interface CQNode {
+    type: 'node'
+    data: {
+      id: number
+    } | {
+      name: string
+      uin: number
+      content: string
+    }
+  }
 
 Channel.extend(()=>({isMeeting:0}))
 Database.extend("koishi-plugin-mysql",({tables})=>{
@@ -13,14 +23,27 @@ Database.extend("koishi-plugin-mysql",({tables})=>{
 }
 )
 
+let cqnodes:CQNode[]=null
+
 async function record(session:Session,next){
     if(!session.channelId) return next()
     try{
-        let {isMeeting} = await session.database.getChannel(session.bot.platform,session.channelId)
+        let isMeeting = (await session.database.getChannel(session.bot.platform,session.channelId)).isMeeting
+        if(isMeeting=1){
+            if(session.bot.platform=='onebot'){
+                let tcqnode:CQNode
+                tcqnode.data={id:Number(session.messageId)}
+                cqnodes.push(tcqnode)
+            }
+            else{
+                session.send("Don't support other platforms now!")
+            }
+        }
     }
     catch(e){
         return next()
     }
+    return next()
 }
 
 export const name='meeting-record'
@@ -34,17 +57,21 @@ export let apply=(ctx:Context) =>{
             // .option('conclusion','-c 添加会议总结')
             .channelFields(['isMeeting']as any)
             .action(async ({session})=>{
-                let {isMeeting} = await ctx.database.getChannel(session.bot.platform,session.channelId)
+                let isMeeting = (await ctx.database.getChannel(session.bot.platform,session.channelId)).isMeeting
                 isMeeting=1
                 return '开始记录！'
             })
             .subcommand('stop')
                 .action(async ({session})=>{
-                    let {isMeeting} = await ctx.database.getChannel(session.bot.platform,session.channelId)
+                    let isMeeting = (await ctx.database.getChannel(session.bot.platform,session.channelId)).isMeeting
                     isMeeting=0
                     session.send('结束记录！')
                     if(session.bot.platform === 'onebot'){
-
+                        let obidfull = (await session.database.getChannel(session.bot.platform,session.channelId)).id
+                        let obid=obidfull.slice(6)
+                        //@ts-expect-error
+                        session.bot.$sendGroupForwardMsg(obid,cqnodes) as any
+                        return
                     }
                 })
     })
