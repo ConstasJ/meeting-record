eaab5151ba792a2dfbbd25dce3852fa803c00224
ConstasJ <2020212726@qq.com>
2021-10-15 19:51:34 +0800

    Huge Changes on index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,36 +13,39 @@ Database.extend("koishi-plugin-mysql",({tables})=>{
 }
 )
 
-let channels=null
-
-let dbUpdate=async (ctx:Context)=>{
-    channels=await ctx.database.getAssignedChannels(['id','isMeeting'])
-}
-
-function record(session:Session){
-
+async function record(session:Session,next){
+    if(!session.channelId) return next()
+    try{
+        let {isMeeting} = await session.database.getChannel(session.bot.platform,session.channelId)
+    }
+    catch(e){
+        return next()
+    }
 }
 
 export const name='meeting-record'
 
 export let apply=(ctx:Context) =>{
-    ctx.command('meet')
-        // .option('introduce','-i 添加会议介绍')
-        // .option('subcon','-sc 添加会议部分总结')
-        // .option('conclusion','-c 添加会议总结')
-        .channelFields(['isMeeting']as any)
-        .action(async ({session})=>{
-            let {isMeeting} = await ctx.database.getChannel(session.bot.platform,session.channelId)
-            isMeeting=1
-            dbUpdate(ctx)
-            ctx.on('message',record)
-            return '开始记录！'
-        }
-        )
-        .subcommand('stop')
+    ctx.on('connect',()=>{
+        ctx.middleware(record,true)
+        ctx.command('meet')
+            // .option('introduce','-i 添加会议介绍')
+            // .option('subcon','-sc 添加会议部分总结')
+            // .option('conclusion','-c 添加会议总结')
+            .channelFields(['isMeeting']as any)
             .action(async ({session})=>{
                 let {isMeeting} = await ctx.database.getChannel(session.bot.platform,session.channelId)
-                isMeeting=0
-            }
-            )
+                isMeeting=1
+                return '开始记录！'
+            })
+            .subcommand('stop')
+                .action(async ({session})=>{
+                    let {isMeeting} = await ctx.database.getChannel(session.bot.platform,session.channelId)
+                    isMeeting=0
+                    session.send('结束记录！')
+                    if(session.bot.platform === 'onebot'){
+
+                    }
+                })
+    })
 }
